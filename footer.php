9928a6d0fb74393662e51fba1052672e4c51b6d0
GerrySinella <gerhardsinella@hotmail.com>
2021-05-20 15:36:57 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
+	<footer id="colophon" class="site-footer oöivnsoöefvsöefvisöklfnsklöefvnseklöfgvsklöefv">
 		<div class="site-info container class">
 			<?php if ( is_active_sidebar( 'footer-sidebar-one' ) ) : ?>
 			<div class="footer-item">
