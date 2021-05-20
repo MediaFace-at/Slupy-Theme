ac7d6f15c7a63a943bd03a824e59c16e79088ee6
GerrySinella <gerhardsinella@hotmail.com>
2021-05-20 14:52:17 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -671,18 +671,6 @@ function slupy_register_required_plugins() {
             //'external_url' => 'https://github.com/thomasgriffin/New-Media-Image-Uploader', // If set, overrides default API URL and points to an external URL.
 		),
         array(
-			'name'               => 'Slupy Plugin', // The plugin name.
-			'slug'               => 'slupy', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/plugins/slupy.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-            
-        ),
-        array(
 			'name'               => 'Contact Form 7', // The plugin name.
 			'slug'               => 'contact-form-7', // The plugin slug (typically the folder name).
 			'source'             => get_template_directory() . '/plugins/contact-form-7.zip', // The plugin source.
